6c108b46b3c4466e12be8135c9c20d0078783e45
Android Studio <devandroid@ort>
2026-08-25 09:04:13 -0300

    soy malo con los nombre de commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,78 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Juego()
+    {
+        return View();
+    }
+
+    public IActionResult Historia()
+    {
+        return View();
+    }
+
+    public IActionResult Integrantes()
+    {
+        return View();
+    }
+
+    public IActionResult Continuar()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Jugar(string usuario, bool continuar)
+    {
+        BD bd = new BD();
+        if (continuar)
+        {
+            if(!bd.RecuperarUsuario(usuario))
+            {
+                ViewBag.Mensaje = "Usuario inexistente o con partida finalizada.";
+                return View("Continuar");
+            }
+            HttpContext.Session.SetString("usuario", usuario);
+            return RedirectToAction("NivelActual");
+        }
+        if (bd.BuscarUsuario(usuario))
+        {
+            ViewBag.Mensaje = "Usuario ya existente.";
+            return View("Juego");
+        }
+        HttpContext.Session.SetString("usuario", usuario);
+        return RedirectToAction("NivelActual");
+    }
+
+    public IActionResult NivelActual()
+    {
+        BD bd = new BD();
+        string usuario = HttpContext.Session.GetString("usuario");
+        int nivel = bd.ObtenerNivel(usuario);
+        return RedirectToAction($"Nivel{nivel}");
+    }
+
+    public IActionResult Nivel1()
+    {
+        return View();
+    }
+    public IActionResult Nivel2()
+    {
+        return View();
+    }
+    public IActionResult Nivel3()
+    {
+        return View();
+    }
+    public IActionResult Nivel4()
+    {
+        return View();
+    }
+    public IActionResult Nivel5()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
---
+++ Models/BD.cs
@@ -0,0 +1,8 @@
+namespace TP06_Martinez_Loufer.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+public class BD
+{
+    private string _connectionString = "Server=localhost;Database= ??? ;Trusted_Connection=True;TrustServerCertificate=True;";
+
+}
